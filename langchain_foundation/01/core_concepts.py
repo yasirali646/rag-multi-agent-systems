c719ae7a93f213b2128d36b49e4db89731591662
Yasir Ali <iamyasirali646@gmail.com>
2026-04-23 23:09:14 +0500

    feat: add batch execution demo

---
+++ langchain_foundation/01/core_concepts.py
@@ -0,0 +1,68 @@
+"""
+LangChain core concepts - LECL and Runnables
+"""
+
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_openrouter import ChatOpenRouter
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+
+def demo_basic_chain(question: str) -> str:
+    """
+        Demonstrates a basic chain using LCEL and Runnables
+
+        Args:
+            question (str): The question to ask the assistant
+        
+        Returns:
+            str: The assistant's response
+    """
+
+
+    prompt = ChatPromptTemplate.from_template(template="You are snarky assistant. Answer in one sentence. {question}") 
+    
+    llm = ChatOpenRouter(model='openai/gpt-oss-120b')
+    
+    parser = StrOutputParser()
+    
+    #Compose with pipe operator
+    #chain = prompt.pipe(llm).pipe(parser)
+    chain = prompt | llm | parser
+
+    #Excute chain with input
+    result = chain.invoke({'question': question})
+
+    return result
+
+def demo_batch_execuation(questions: list[dict]):
+    """
+        Demonstrates batch execution of a chain using LCEL and Runnables
+
+        Args:
+            questions (list[dict]): A list of questions to ask the assistant
+        
+        Returns:
+            None
+    """
+    prompt = ChatPromptTemplate.from_template(template="Translate to Italian : {text}")
+    llm = ChatOpenRouter(model='openai/gpt-oss-120b')
+    parser = StrOutputParser()
+
+    chain = prompt | llm | parser
+
+    result = chain.batch(questions)
+
+    for res in zip(result, questions):
+        print(f"Input: {res[1]['text']} --> Output: {res[0]}")
+
+demo_batch_execuation([
+    {'text': 'Hello, how are you?'},
+    {'text': 'What is your name?'},
+    {'text': 'What is the capital of France?'},
+    {'text': 'What is the meaning of life?'},
+    {'text': 'What is the airspeed of a laden swallow?'}
+])
